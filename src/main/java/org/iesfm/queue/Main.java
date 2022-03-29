7e02f9fa40464816dfd6256f6133efd37c53053e
javibucrack174@gmail.com <javibucrack174@gmail.com>
2022-03-29 12:18:20 +0200

    Queue done

--- src/main/java/org/iesfm/queue/Main.java
+++ src/main/java/org/iesfm/queue/Main.java
@@ -18,5 +18,13 @@ public class Main {
         queue2.enqueue("Bye");
         String v2 = queue2.dequeue();
         System.out.println(v2);
+
+        Queue<Boolean> queue3 = new Queue<>();
+        queue3.enqueue(true);
+        queue3.enqueue(false);
+        queue3.enqueue(false);
+        queue3.enqueue(true);
+        boolean v3 = queue3.dequeue();
+        System.out.println(v3);
     }
 }
